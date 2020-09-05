43580f93a41629889bd7b8d0817620b910f93a62
DiazRock <corolariodiaz@gmail.com>
2020-09-05 16:42:59 -0400

    added the crown_suite_test.go with ginkgo

---
+++ chain/crown/crown_suite_test.go
@@ -0,0 +1,13 @@
+package crown_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"testing"
+)
+
+func TestCrown(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Crown Suite")
+}
