69642dee4e989f38f2b46543f226d81a54c06cb4
DiazRock <corolariodiaz@gmail.com>
2020-09-07 16:40:28 -0400

    I can run the container and It gives a network error before running the test

--- infra/crown/run.sh
+++ infra/crown/run.sh
@@ -1,3 +1,15 @@
+#!/bin/bash
+ADDRESS=$1
+# Print setup
+echo "CROWN_ADDRESS=$ADDRESS"
+
 # Start
 /app/bin/crownd
-sleep 10
+sleep 100
+
+
+# Import the address
+/app/bin/crown-cli importaddress $ADDRESS
+
+
+
