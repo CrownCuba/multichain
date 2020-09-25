2b805c9b33ecd7dd6e79da44dc97dcb39f8859c5
Adrian Hernandez <adrianhdez929@gmail.com>
2020-09-25 01:48:49 -0400

    Adding gas estimator implementation

--- chain/crown/gas.go
+++ chain/crown/gas.go
@@ -1,11 +1,19 @@
 package crown
 
 import (
-	"github.com/renproject/multichain/chain/bitcoin"
+	"context" 
+	"math/big"  
 	"github.com/renproject/pack"
 )
-type GasEstimator = bitcoin.GasEstimator
 
 // Fixed fee amount since Crown doesn't allows too high fees
 
-var Fee = pack.NewU64(100000)
+var Gas = big.NewInt(10000)
+
+type Estimator struct {} 
+
+// Implementation to get the real Gas from fees
+
+func (Estimator) EstimateGasPrice(ctx context.Context) (pack.U256, error) { 
+return pack.NewU256FromInt(Gas), nil
+}
